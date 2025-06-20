053c41e654b5fe8e4cb655d157bfdb491dc4e509
uunsetiawati <50813525+uunsetiawati@users.noreply.github.com>
2025-06-20 13:17:15 +0700

    status di pengumuman model

--- app/Models/Pengumuman_m.php
+++ app/Models/Pengumuman_m.php
@@ -11,7 +11,18 @@ class Pengumuman_m extends Model
     protected $allowedFields = ['judul', 'deskripsi', 'gambar', 'yt','status', 'created_at', 'updated_at'];
     protected $useTimestamps = true;
 
-    
+    public function getEnumValues($field)
+    {
+        $query = $this->db->query("SHOW COLUMNS FROM $this->table LIKE '$field'");
+        $row = $query->getRow();
+        preg_match('/^enum\((.*)\)$/', $row->Type, $matches);
+        $enum = [];
+        foreach (explode(',', $matches[1]) as $value) {
+            $v = trim($value, "'");
+            $enum[] = $v;
+        }
+        return $enum;
+    }
 }
 
 
